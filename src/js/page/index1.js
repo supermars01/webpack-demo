5bb55632c679a686ba37c5fe7ee3d9b121c40b03
supermars01 <kings_821113@163.com>
2016-10-20 12:01:20 +0800

    update webpack-config.build.js

---
+++ src/js/page/index1.js
@@ -0,0 +1,9 @@
+!function(){
+	// 引入公共css模块
+	require('../../css/vendor/reset.css');
+	require('../../css/module/footer.css');
+    require('../../css/sprite-base.css');
+	require('../../css/page/index1.scss');
+	// 加载 header模块
+	require('../module/header.js');
+}()
